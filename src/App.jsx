6d67544064cf0036f496a6c0f84217d476554f6d
AsrarOthman <asrarothman@gmail.com>
2023-07-21 23:09:36 +0800

    repo

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import wgpsLogo from "./assets/logo.ong.png";
 function App() {
   return (
     <>
-      <div className="bp1 frr ">
-        <div className="bp5 fcse ai-c">
+      <div className="bp1 frr  ai-c">
+        <div className="bp5 fcse ai-c ">
           <div className="bp4 fcc tx-c tx-main">
             <a>Welcome To Our Telematic System</a>
           </div>
-          <div className="bp4 tx-c">
-            <a style={{ fontSize: "20px" }}>
+          <div className="bp4 tx-c tx-size-1">
+            <a>
               This is our innovative system in 2023, where we have integrated
               our telematic systems with the cutting-edge WhatsGPS Platform to
               provide exceptional service to our valued clients.
@@ -29,17 +29,15 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="bp4 fcb tx-c">
-            <a className="">Copyright@2023AoTechology</a>
+          <div className="bp4 fcb tx-c mg-30">
+            <h4 className="">Copyright@2023AoTechology</h4>
           </div>
         </div>
-        <div className="bp2 fcc ai-c ">
-          <div className="bp3 boxs-1 fcse ai-c">
+        <div className="bp2 fcc ai-c  ">
+          <div className="bp3 boxs-1 fcse ai-c ">
             <div className="bp9 tx-main tx-c fcc" style={{ fontSize: "20px" }}>
-              <a className="mg-10" style={{ fontSize: "23px" }}>
-                GPS Telematic System
-              </a>
-              <a>
+              <a className=" tx-size-2 mg-30">GPS Telematic System</a>
+              <a className="tx-size-3 mg-20">
                 You are invited to access our platform by clicking the login
                 button below.
               </a>
@@ -55,6 +53,9 @@ function App() {
                 </a>
               </div>
             </div>
+          </div>
+          <div className="bp4-1 fcb tx-c">
+            <h3 className="tx-size-4">Copyright@2023AoTechology</h3>
           </div>
         </div>
       </div>
